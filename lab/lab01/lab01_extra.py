b219c16a666968524165ae4c70f4ee608f758bbb
David Culler <culler@berkeley.edu>
2016-01-31 18:42:29 -0800

    Updated, tested and released lab01 Updated and released hw02.  Still needs additional testing.

---
+++ lab/lab01/lab01_extra.py
@@ -0,0 +1,32 @@
+"""Coding practice for Lab 1."""
+
+# While Loops
+
+def factors(n):
+    """Prints out all of the numbers that divide `n` evenly.
+
+    >>> factors(20)
+    20
+    10
+    5
+    4
+    2
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    "*** YOUR CODE HERE ***"
+
