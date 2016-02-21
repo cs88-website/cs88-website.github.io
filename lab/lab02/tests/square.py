a357920b8ecb20b2d57ee0f3ed72939d77e4561a
David Culler <culler@berkeley.edu>
2016-02-20 16:17:22 -0800

    Initial release of lab03 and hw03.  Small fixes remain

--- lab/lab02/tests/square.py
+++ lab/lab02/tests/square.py
@@ -8,12 +8,8 @@ test = {
           'code': r"""
           >>> def square(x):
           ...     return x * x
-          53cf1f506826c54971339dcc19807024
-          # locked
           >>> def neg(f, x):
           ...     return -f(x)
-          53cf1f506826c54971339dcc19807024
-          # locked
           >>> neg(square, 4)
           16b16382e565d37cf26464067a3e8375
           # locked
