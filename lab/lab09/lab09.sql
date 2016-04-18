14eb67ad372f0a1e2699b02f31721ee87c4de1bc
David Culler <culler@berkeley.edu>
2016-04-18 00:32:48 -0700

    added lab09

---
+++ lab/lab09/lab09.sql
@@ -0,0 +1,38 @@
+create table parents as
+  select "abraham" as parent, "barack" as child union
+  select "abraham"          , "clinton"         union
+  select "delano"           , "herbert"         union
+  select "fillmore"         , "abraham"         union
+  select "fillmore"         , "delano"          union
+  select "fillmore"         , "grover"          union
+  select "eisenhower"       , "fillmore";
+
+create table dogs as
+  select "abraham" as name, "long" as fur, 26 as height union
+  select "barack"         , "short"      , 52           union
+  select "clinton"        , "long"       , 47           union
+  select "delano"         , "long"       , 46           union
+  select "eisenhower"     , "short"      , 35           union
+  select "fillmore"       , "curly"      , 32           union
+  select "grover"         , "short"      , 28           union
+  select "herbert"        , "curly"      , 31;
+
+create table sizes as
+  select "toy" as size, 24 as min, 28 as max union
+  select "mini",        28,        35        union
+  select "medium",      35,        45        union
+  select "standard",    45,        60;
+-- All short dogs
+create table short_dogs as
+-- REPLACE THIS LINE
+-- The size of each dog
+create table size_of_dogs as
+-- REPLACE THIS LINE
+
+-- All dogs with parents ordered by decreasing height of their parent
+create table by_height as
+-- REPLACE THIS LINE
+
+create table tallest as
+-- REPLACE THIS LINE
+
