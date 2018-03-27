f148b6b90cac3537214415f51623e5b7e978219f
jkpvecino <jkpvecino@gmail.com>
2018-03-26 21:34:30 -0700

    released solns

---
+++ hw/sol-hw01/hw01.py
@@ -0,0 +1,29 @@
+"""Homework 1."""
+
+def both_positive(x, y):
+    """Returns True if both x and y are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return x > 0 and y > 0
+
+
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b) # You can replace this line, but don't have to.
+
---
+++ hw/sol-hw06/hw06.py
@@ -0,0 +1,114 @@
+# generators
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+def scale(s, k):
+    """Yield elements of the iterable s scaled by a number k.
+
+    >>> s = scale([1, 5, 2], 5)
+    >>> type(s)
+    <class 'generator'>
+    >>> list(s)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    for elem in s:
+        yield elem * k
+
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    i = n
+    while i > 1:
+        yield i
+        if i % 2 == 0:
+            i //= 2
+        else:
+            i = i * 3 + 1
+    yield i
+
+
+def merge(s0, s1):
+    """Yield the elements of strictly increasing iterables s0 and s1 and 
+    make sure to remove the repeated values in both. 
+    You can also assume that s0 and s1 represent infinite sequences.
+
+    >>> twos = scale(naturals(), 2)
+    >>> threes = scale(naturals(), 3)
+    >>> m = merge(twos, threes)
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    """
+    i0, i1 = iter(s0), iter(s1)
+    e0, e1 = next(i0), next(i1)
+    while True:
+        yield min(e0, e1)
+        if e0 < e1:
+            e0 = next(i0)
+        elif e1 < e0:
+            e1 = next(i1)
+        else:
+            e0, e1 = next(i0), next(i1)
+
+
+
+def remainders_generator(m):
+    """
+    Takes in an integer m, and yields m different remainder groups
+    of m.
+
+    >>> remainders_mod_four = remainders_generator(4)
+    >>> for rem_group in remainders_mod_four:
+    ...     for _ in range(3):
+    ...         print(next(rem_group))
+    0
+    4
+    8
+    1
+    5
+    9
+    2
+    6
+    10
+    3
+    7
+    11
+    """
+    def remainder_group(rem):
+        start = rem
+        while True:
+            yield start
+            start += m
+
+    for rem in range(m):
+        yield remainder_group(rem)
+
