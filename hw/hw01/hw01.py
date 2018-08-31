ad0bbaabf7d5361d911b69f081a2436027ad599e
Jessica Ping Gao <gaojessicaping@berkeley.edu>
2018-08-31 10:14:11 -0700

    changed lab01

--- hw/hw01/hw01.py
+++ hw/hw01/hw01.py
@@ -1,14 +1,72 @@
 """Homework 1."""
 
-def both_positive(x, y):
-    """Returns True if both x and y are positive.
+def diff(x, y, z):
+    """Return whether one argument is the difference between the other two.
 
-    >>> both_positive(-1, 1)
-    False
-    >>> both_positive(1, 1)
+    x, y, and z are all integers.
+
+    >>> diff(5, 3, 2) # 5 - 3 is 2
     True
+    >>> diff(2, 3, 5) # 5 - 3 is 2
+    True
+    >>> diff(2, 5, 3) # 5 - 3 is 2
+    True
+    >>> diff(-2, 3, 5) # 3 - 5 is -2
+    True
+    >>> diff(-5, -3, -2) # -5 - -2 is -3
+    True
+    >>> diff(-2, 3, -5) # -2 - 3 is -5
+    True
+    >>> diff(2, 3, -5)
+    False
+    >>> diff(10, 6, 4)
+    True
+    >>> diff(10, 6, 3)
+    False
+
+
     """
-    return x and y > 0 # You can replace this line!
+    "*** YOUR CODE HERE ***"
+
+
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+    """Calculates the Euclidian distance between two points (x1, y1) and (x2, y2)
+
+    >>> distance(1, 1, 1, 2)
+    1.0
+    >>> distance(1, 3, 1, 1)
+    2.0
+    >>> distance(1, 2, 3, 4)
+    2.8284271247461903
+    """
+    "*** YOUR CODE HERE ***"
+
+def distance3d(x1, y1, z1, x2, y2, z2):
+    """Calculates the 3D Euclidian distance between two points (x1, y1, z1) and
+    (x2, y2, z2).
+
+    >>> distance3d(1, 1, 1, 1, 2, 1)
+    1.0
+    >>> distance3d(2, 3, 5, 5, 8, 3)
+    6.164414002968976
+    """
+    "*** YOUR CODE HERE ***"
+
+
+from math import sqrt
+
+def quadratic(a,b,c):
+    """
+    >>> quadratic(1,0,-1)
+    (-1.0, 1.0)
+    >>> quadratic(1,2,1)
+    (-1.0, -1.0)
+    >>> quadratic(1,3,-4)
+    (-4.0, 1.0)
+    """
+    "*** YOUR CODE HERE ***"
 
 
 from operator import add, sub
