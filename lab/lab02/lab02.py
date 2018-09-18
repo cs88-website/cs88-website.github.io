e82089a51ac6fa4ddeb1b5eafca755bb50ca7463
Jessica Ping Gao <gaojessicaping@berkeley.edu>
2018-09-18 10:56:56 -0700

    LA updates

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -3,30 +3,56 @@
 ## Coding
 
 def a_or_c(grade):
-	"""
-	We all know the saying "C's get degrees".
-	We all would like to get an A, but sometimes
-	a C will have to do. 
+    """
+    We all know the saying "C's get degrees".
+    We all would like to get an A, but sometimes
+    a C will have to do. 
 
-	Return whether the grade inputted
-	would receive an A or C.
+    Return whether the grade inputted
+    would receive an A or C.
 
-	>>> a_or_c(100)
-	True
-	>>> a_or_c(75)
-	True
-	>>> a_or_c(82)
-	False
-	>>> a_or_c(80)
-	False
-	>>> a_or_c(95)
-	True
-	>>> a_or_c(40)
-	False
-	"""
-	#YOUR CODE HERE
-	return False
+    >>> a_or_c(100)
+    True
+    >>> a_or_c(75)
+    True
+    >>> a_or_c(82)
+    False
+    >>> a_or_c(80)
+    False
+    >>> a_or_c(95)
+    True
+    >>> a_or_c(40)
+    False
+    """
+    #YOUR CODE HERE
+    return False
 
+
+
+## Control
+
+def min(x, y):
+    """
+    Return the minimum between x and y
+
+    >>> min(1,2)
+    1
+    >>> min(3,1)
+    1
+    >>> min(2,3)
+    2
+    >>> min(0, 67777)
+    0
+    >>> min(-1, -5)
+    -5
+    >>> min(-7, -1)
+    -7
+    >>> min(0, 0)
+    0
+    """
+
+    # YOUR CODE HERE
+    return 0
 
 
 ## Transformation
@@ -52,53 +78,27 @@ def abs_value_equal(x, y):
     False
     """
     # YOUR CODE HERE
- 	return False
+    return False
 
 
 ## Representation
 
 def mirror(num1, num2):
-	"""
-	Return if num1 is num2 backwards
-	The inputs will always be < 100.
+    """
+    Return if num1 is num2 backwards
+    The inputs will always be < 100.
 
-	>>> mirror(12, 21)
-	True
-	>>> mirror(54, 45)
-	True
-	>>> mirror(33, 33)
-	True
-	>>> mirror(42, 52)
-	False
-	>>> mirror(12, 22)
-	False
-	"""
-	# YOUR CODE HERE
-	return False
-
-
-## Control
-
-def min(x, y):
-	"""
-	Return the minimum between x and y
-
-	>>> min(1,2)
-	1
-	>>> min(3,1)
-	1
-	>>> min(2,3)
-	2
-	>>> min(0, 67777)
-	0
-	>>> min(-1, -5)
-	-1
-	>>> min(-7, -1)
-	-7
-	>>> min(0, 0)
-	0
-	"""
-
-	# YOUR CODE HERE
-	return 0
+    >>> mirror(12, 21)
+    True
+    >>> mirror(54, 45)
+    True
+    >>> mirror(33, 33)
+    True
+    >>> mirror(42, 52)
+    False
+    >>> mirror(12, 22)
+    False
+    """
+    # YOUR CODE HERE
+    return False
 
