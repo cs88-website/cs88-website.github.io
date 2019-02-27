94571765d14b6162359cca7ffaae405bcd40d96d
Andrew Tan <andrewztan12@gmail.com>
2019-02-27 03:05:12 -0800

    added slides and solutions

---
+++ hw/sol-hw03/hw03.py
@@ -0,0 +1,118 @@
+######################
+# Required Questions #
+######################
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 10)  # 4 * 3 * 2 * 1 # Only n times!!
+    24
+    """
+    if k > n:
+        k = n
+    total, stop = 1, n-k
+    while n > stop:
+        total, n = total*n, n-1
+    return total
+
+
+def nonzero(lst):
+    """ Returns the first nonzero element of a list
+
+    >>> nonzero([1, 2, 3])
+    1
+    >>> nonzero([0, 1, 2])
+    1
+    >>> nonzero([0, 0, 0, 0, 0, 0, 5, 0, 6])
+    5
+    """
+    for i in lst:
+        if i != 0:
+            return i
+
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    s = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        print(n)
+        s = s + 1
+    return s
+
+
+def odd_even(x):
+    """Classify a number as odd or even.
+    
+    >>> odd_even(4)
+    'even'
+    >>> odd_even(3)
+    'odd'
+    """
+    if (x % 2) == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+def classify(s):
+    """
+    Classify all the elements of a sequence as odd or even
+    >>> classify([0, 1, 2, 4])
+    ['even', 'odd', 'even', 'even']
+    """
+    return [odd_even(x) for x in s]
+
+
+def decode_helper(pair):
+    """
+    Optional helper function! Could be useful to turn something like [0, 0] to 'Male 0-9'
+    """
+    result = ""
+    if pair[0] == 0:
+        result += "Male "
+    else:
+        result += "Female "
+
+    if pair[1] == 0:
+        result += '0-9'
+    elif pair[1] == 10:
+        result += '100+'
+    else:
+        result += str(pair[1]) + '0-' + str(pair[1]) + '9'
+    return result
+    
+
+def decode(list_of_sex_age_pairs):
+    """
+    >>> decode([[0, 0], [1, 1], [1, 10]])
+    ['Male 0-9', 'Female 10-19', 'Female 100+']
+    >>> decode([[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [1, 10]])
+    ['Male 0-9', 'Male 10-19', 'Male 20-29', 'Male 30-39', 'Male 40-49', 'Female 50-59', 'Female 60-69', 'Female 70-79', 'Female 80-89', 'Female 90-99', 'Female 100+']
+    """ 
+    return [decode_helper(pair) for pair in list_of_sex_age_pairs]
+
---
+++ lab/sol-lab02/lab02.py
@@ -0,0 +1,103 @@
+"""Lab 2: Functions """
+
+## Coding
+
+def a_or_c(grade):
+    """
+    We all know the saying "C's get degrees".
+    We all would like to get an A, but sometimes
+    a C will have to do. 
+
+    Return whether the grade inputted
+    would receive an A or C.
+
+    >>> a_or_c(100)
+    True
+    >>> a_or_c(75)
+    True
+    >>> a_or_c(82)
+    False
+    >>> a_or_c(80)
+    False
+    >>> a_or_c(95)
+    True
+    >>> a_or_c(40)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+## Control
+
+def min(x, y):
+    """
+    Return the minimum between x and y
+
+    >>> min(1,2)
+    1
+    >>> min(3,1)
+    1
+    >>> min(2,3)
+    2
+    >>> min(0, 67777)
+    0
+    >>> min(-1, -5)
+    -5
+    >>> min(-7, -1)
+    -7
+    >>> min(0, 0)
+    0
+    """
+    "*** YOUR CODE HERE ***"
+
+
+## Transformation
+
+def abs_value_equal(x, y):
+    """Return whether or not the absolute value of both numbers is the same.
+
+    Please refrain from using libraries (abs)
+
+    >>> abs_value_equal(-2, -2)
+    True
+    >>> abs_value_equal(-3, 3)
+    True
+    >>> abs_value_equal(1, 2)
+    False
+    >>> abs_value_equal(3, 3)
+    True
+    >>> abs_value_equal(-6, -6)
+    True
+    >>> abs_value_equal(-1, -5)
+    False
+    >>> abs_value_equal(5, -6)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+## Representation
+
+def mirror(num1, num2):
+    """
+    Return if num1 is num2 backwards
+    The inputs will always be < 100.
+
+    >>> mirror(12, 21)
+    True
+    >>> mirror(54, 45)
+    True
+    >>> mirror(33, 33)
+    True
+    >>> mirror(42, 52)
+    False
+    >>> mirror(12, 22)
+    False
+    >>> mirror(19, 91)
+    True
+    >>> mirror(5, 6)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    
+
