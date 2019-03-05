ec046479c899406b9e72ec567af88f9121d70ac2
Andrew Tan <andrewztan12@gmail.com>
2019-03-04 22:08:28 -0800

    lab 6, hw 6, lecture

--- hw/hw06/hw06.py
+++ hw/hw06/hw06.py
@@ -1,19 +1,4 @@
 
-def deep_len(lst):
-    """Returns the deep length of the list.
-
-    >>> deep_len([1, 2, 3])     # normal list
-    3
-    >>> x = [1, [2, 3], 4]      # deep list
-    >>> deep_len(x)
-    4
-    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
-    >>> deep_len(x)
-    6
-    """
-    "*** YOUR CODE HERE ***"
-
-
 def interval(a, b):
     """Construct an interval from a to b."""
     "*** YOUR CODE HERE ***"
