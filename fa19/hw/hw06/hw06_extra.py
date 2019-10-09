d08ec7288db105e37d95c00aeb5317e9a24000af
Alex Kassil <alexkassil@sbcglobal.net>
2019-10-09 01:36:48 -0700

    Deploy website Wed Oct  9 01:36:47 PDT 2019

---
+++ fa19/hw/hw06/hw06_extra.py
@@ -0,0 +1,23 @@
+# Challenge
+
+def decimal(n):
+    """Return a list representing the decimal representation of a number.
+
+    >>> decimal(55055)
+    [5, 5, 0, 5, 5]
+    >>> decimal(-136)
+    ['-', 1, 3, 6]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def binary(n):
+    """Return a list representing the representation of a number in base 2.
+
+    >>> binary(55055)
+    [1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+    >>> binary(-136)
+    ['-', 1, 0, 0, 0, 1, 0, 0, 0]
+    """
+    "*** YOUR CODE HERE ***"
+
