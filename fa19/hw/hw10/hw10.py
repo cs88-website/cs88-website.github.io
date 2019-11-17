aa2144ded9998d57e345ca503a109cf90e533a36
Amir Shahatit <ashahatit@berkeley.edu>
2019-11-16 16:54:49 -0800

    HW10 push

--- fa19/hw/hw10/hw10.py
+++ fa19/hw/hw10/hw10.py
@@ -1,54 +1,115 @@
+# Linked List Class
+class Link:
+    """
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s
+    Link(1, Link(2, Link(3)))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(repr(self.first), rest_str)
+
+    def __len__(self):
+        """ Return the number of items in the linked list.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> len(s)
+        3
+        >>> s = Link.empty
+        >>> len(s)
+        0
+        """
+        return 1 + len(self.rest)
+
+    def __getitem__(self, i):
+        """Returning the element found at index i.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> s[1]
+        2
+        >>> s[2]
+        3
+        """
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
+
+def print_link(link):
+    """Print elements of a linked list link.
+
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> print_link(link)
+    <1 2 3>
+    >>> link1 = Link(1, Link(Link(2), Link(3)))
+    >>> print_link(link1)
+    <1 <2> 3>
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> print_link(link1)
+    <3 <4> 5 6>
+    """
+    print('<' + helper(link).rstrip() + '>')
+
+def helper(link):
+    if link == Link.empty:
+        return ''
+    elif isinstance(link.first, Link):
+        return '<' + helper(link.first).rstrip() + '> ' + helper(link.rest)
+    else:
+        return str(link.first) +' '+  helper(link.rest)
+
+# Tree Class
+class Tree:
+    def __init__(self, entry, branches=()):
+        self.entry = entry
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branches_str = ', ' + repr(self.branches)
+        else:
+            branches_str = ''
+        return 'Tree({0}{1})'.format(self.entry, branches_str)
+
+    def is_leaf(self):
+        return not self.branches
+
+
+
 ################
 #### Trees #####
 ################
 
-def cumulative_sum(t):
-    """Return a new Tree, where each entry is the sum of all entries in the
-    corresponding subtree of t.
+def leaves(t):
+    """Returns a list of all the entries of the leaf nodes of the Tree t.
 
-    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
-    >>> cumulative = cumulative_sum(t)
-    >>> t
-    Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
-    >>> cumulative
-    Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
-    >>> cumulative_sum(Tree(1))
-    Tree(1)
+    >>> leaves(Tree(1))
+    [1]
+    >>> leaves(Tree(1, [Tree(2, [Tree(3)]), Tree(4)]))
+    [3, 4]
     """
     "*** YOUR CODE HERE ***"
 
 
-def add_d_leaves(t, v):
-    """Add d leaves containing v to each node at every depth d.
+def square_tree(t):
+    """Mutates a Tree t by squaring all its elements.
 
-    >>> t1 = Tree(1, [Tree(3)])
-    >>> add_d_leaves(t1, 4)
-    >>> t1
-    Tree(1, [Tree(3, [Tree(4)])])
-    >>> t2 = Tree(2, [Tree(5), Tree(6)])
-    >>> t3 = Tree(3, [t1, Tree(0), t2])
-    >>> add_d_leaves(t3, 10)
-    >>> print(t3)
-    3
-      1
-        3
-          4
-            10
-            10
-            10
-          10
-          10
-        10
-      0
-        10
-      2
-        5
-          10
-          10
-        6
-          10
-          10
-        10
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> square_tree(t)
+    >>> t
+    Tree(1, [Tree(9, [Tree(25)]), Tree(49)])
     """
     "*** YOUR CODE HERE ***"
 
@@ -73,21 +134,17 @@ def deep_map(f, link):
     "*** YOUR CODE HERE ***"
 
 
-def every_other(s):
-    """Mutates a linked list so that all the odd-indiced elements are removed
-    (using 0-based indexing).
+def reverse(link):
+    """Returns a Link that is the reverse of the original.
 
-    >>> s = Link(1, Link(2, Link(3, Link(4))))
-    >>> every_other(s)
-    >>> s
-    Link(1, Link(3))
-    >>> odd_length = Link(5, Link(3, Link(1)))
-    >>> every_other(odd_length)
-    >>> odd_length
-    Link(5, Link(1))
-    >>> singleton = Link(4)
-    >>> every_other(singleton)
-    >>> singleton
-    Link(4)
+    >>> print_link(reverse(Link(1)))
+    <1>
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> new = reverse(link)
+    >>> print_link(new)
+    <3 2 1>
+    >>> print_link(link)
+    <1 2 3>
     """
     "*** YOUR CODE HERE ***"
+
