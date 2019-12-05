706fef8220d8a1ba99d667026de3a8a7e15d26c7
Alex Kassil <alexkassil@sbcglobal.net>
2019-12-05 15:50:48 -0800

    Deploy website Thu Dec  5 15:50:46 PST 2019

--- fa19/lab/lab11/lab11.py
+++ fa19/lab/lab11/lab11.py
@@ -1,3 +1,21 @@
+# Naturals
+
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
 # Iterators: Scale
 
 class ScaleIterator:
